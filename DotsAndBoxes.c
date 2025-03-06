02c4b2119d4cacd8571cee7666b13a5873a435f8
Mazen Hachem <mazen-hachem@outlook.com>
2025-03-06 21:19:02 +0200

    initialized some variables current player, lines, scores etcc

--- DotsAndBoxes.c
+++ DotsAndBoxes.c
@@ -1,10 +1,19 @@
+#include <stdbool.h>
+
+typedef struct {
+    // Grid state
+    bool horizontal_lines[4][5];  // 4 rows, 5 cols of horizontal lines
+    bool vertical_lines[5][4];    // 5 rows, 4 cols of vertical lines
+    int box_owner[4][5];          // 0=neutral, 1/2=players
+    
+    // Game state
+    int current_player;
+    int scores[2];
+    int remaining_boxes;
+} GameState;
 
 int main() {
     //main function
-    // hi
-
     printf("test");
-
-    //test branch this should be in branch
     return 0;
 }
