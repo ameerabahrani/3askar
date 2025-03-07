868f64c00c45b4e68059af322e6dca87352e6498
HashemAwad <Hia33@mail.aub.edu>
2025-03-07 20:06:06 +0200

    header files

--- DotsAndBoxes.c
+++ DotsAndBoxes.c
@@ -1,20 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
-
-//include all files used
 #include "game.h"
-#include "PrintMethod.h"
-                                       // function called from another file PrintMethod.c
-void PrintBoard(char board[9][11]);
 
-typedef struct {
-    bool horizontal_lines[4][5];      //horizontal lines
-    bool vertical_lines[5][4];       //vertical lines
-    int box_owner[4][5];            // 0=neutral  1/2=players
-    int current_player;
-    int scores[2];
-    char board[9][11];            //9×11 board for printing dots and spaces
-} GameState;
 
 int main() {
                                 
--- game.h
+++ game.h
@@ -1,25 +1,29 @@
 #ifndef game_h
 #define game_h
 
+#include "Line.c"
+#include "Logic.c"
+#include "PrintMethod.c"
 #include <stdbool.h>
 
 #define ROWS 4
 #define COLS 5
+#define players 2
 
 typedef struct {
-    bool horizontal_lines[ROWS+1][COLS];  
-    bool vertical_lines[COLS+1][ROWS];  
-    int box_owner[ROWS][COLS];          // 0=neutral, 1/2=players
-    
-
+    bool horizontal_lines[ROWS + 1][COLS];      //horizontal lines
+    bool vertical_lines[COLS + 1][ROWS];       //vertical lines
+    int box_owner[ROWS][COLS];            // 0=neutral  1/2=players
     int current_player;
-    int scores[2];
+    int scores[players];
     int remaining_boxes;
+    char board[ROWS * 2 + 1][COLS * 2 + 1];            //9×11 board for printing dots and spaces
 } GameState;
 
 int line_type(int r1, int c1, int r2, int c2);
 bool adjacent (int r1, int c1, int r2, int c2);
 bool check_box(GameState *state, int r, int c);
 int process_move(GameState *state, int r1, int c1, int r2, int c2);
+void PrintBoard(char board[9][11]);
 
 #endif
